75ada4febc648f0982697034efdab79bc1c4b3d7
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-11-22 01:24:18 +0200

    Refactored to work with vliw instructions

--- lab5/src/system/processor.ts
+++ lab5/src/system/processor.ts
@@ -1,32 +1,39 @@
+import { Instruction } from './types/Instruction';
 import { randomUUID } from 'node:crypto';
 
 export default class Processor {
   readonly id = randomUUID();
   private currentLoad: number;
+  private busy: boolean;
 
   constructor() {
-    this.currentLoad = 0; // Initialize load to 0
+    this.currentLoad = 0;
+    this.busy = false;
   }
 
-  async executeOperation(
-    op: string,
-    left: number,
-    right: number,
-  ): Promise<number> {
+  isBusy(): boolean {
+    return this.busy;
+  }
+
+  async executeOperation(instruction: Instruction): Promise<number> {
+    const { op, leftOperand, rightOperand } = instruction;
     const timeRequired = this.getOperationTime(op);
+
+    this.busy = true;
     this.currentLoad += timeRequired;
-    await this.delay(timeRequired);
+    await this.delay(timeRequired); // Simulate execution time
     this.currentLoad -= timeRequired;
+    this.busy = false;
 
     switch (op) {
       case '+':
-        return left + right;
+        return leftOperand + rightOperand;
       case '-':
-        return left - right;
+        return leftOperand - rightOperand;
       case '*':
-        return left * right;
+        return leftOperand * rightOperand;
       case '/':
-        return left / right;
+        return leftOperand / rightOperand;
       default:
         throw new Error(`Unsupported operation: ${op}`);
     }
@@ -43,11 +50,11 @@ export default class Processor {
     return timings[op as keyof typeof timings] || 1;
   }
 
-  getCurrentLoad(): number {
-    return this.currentLoad;
-  }
-
   private delay(ms: number): Promise<void> {
     return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  get processorLoad() {
+    return this.currentLoad;
   }
 }
