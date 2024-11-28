4efd75bf717bef64e64eedbb593cb64ef291e82d
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-11-28 20:52:15 +0200

    Small output fix

--- lab3-4/src/utils/processResults.ts
+++ lab3-4/src/utils/processResults.ts
@@ -6,21 +6,29 @@ import { treeToInfix, verifyTransformation } from './verifyTransformation';
 export const processResults = async (
   conversion: 'Commutative' | 'Distributive',
   expressionTree: TreeNode,
-  tress: TreeNode[],
+  trees: TreeNode[] | TreeNode,
   expression: string,
   expressionIndex: number,
 ) => {
   console.log(`${conversion} trees of ${expression}:`);
   const tests = [];
-  for (const tree of tress) {
-    const graph = outputTreeGraph(tree);
-    await convertDotToPng(
-      graph!,
-      `${conversion}Graph${expressionIndex}-${tress.indexOf(tree)}`,
-    );
-    tests.push(verifyTransformation(expressionTree!, tree, expression));
-    console.log(treeToInfix(tree));
+  if (!(trees instanceof Array)) {
+    const graph = outputTreeGraph(trees);
+    await convertDotToPng(graph!, `${conversion}Graph${expressionIndex}`);
+    tests.push(verifyTransformation(expressionTree!, trees, expression));
+    console.log(treeToInfix(trees));
+  } else {
+    for (const tree of trees) {
+      const graph = outputTreeGraph(tree);
+      await convertDotToPng(
+        graph!,
+        `${conversion}Graph${expressionIndex}-${trees.indexOf(tree)}`,
+      );
+      tests.push(verifyTransformation(expressionTree!, tree, expression));
+      console.log(treeToInfix(tree));
+    }
   }
+
   if (tests.every(result => result)) {
     console.log(
       `${conversion} conversion of ${expression} is correct for all test cases`,
